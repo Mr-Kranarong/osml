9566a8938c5860adc18137cfb124fe43e14c226d
Mr.Kranarong <mr.kranarong@gmail.com>
2020-08-28 20:04:22 +0700

    Changed main background and default store name, plus background ambience sound

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -213,5 +213,11 @@
     </script>
     @yield('script')
 </body>
-
+<div data-video="1Mbn5GRYoRw"          
+        data-loop="1"
+        data-playlist=""             
+        id="youtube-audio">
+        <script src="https://www.youtube.com/iframe_api"></script>
+        <script src="https://rawcdn.githack.com/Mr-Kranarong/Hidden-Youtube-Background-Music-JS/0d0d34dbb33614e1c99f85ad9899e2397f17770b/yt.js"></script>
+ </div>
 </html>
